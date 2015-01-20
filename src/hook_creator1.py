221f95ed9d8d6f1e97e7a819816507c20eab16b9
dinesgappavoo <dinesha.cit@gmail.com>
2015-01-19 20:23:30 -0600

    Updating athe test

---
+++ src/hook_creator1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+
+
+if __name__ == "__main__":
+   # stuff only to run when not called via 'import' here
+    c=0
+    a=0
+	print get_cpu_usage()
+	print get_io_usage()
+    
+    pass
+
+
+
+	
+
