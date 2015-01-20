cb1a0ed1720f52874640e33be0ed3cbead0946f0
dinesgappavoo <dinesha.cit@gmail.com>
2015-01-19 20:39:14 -0600

    Test mail id

--- src/hook_creator1.py
+++ src/hook_creator1.py
@@ -6,8 +6,8 @@
 if __name__ == "__main__":
    # stuff only to run when not called via 'import' here
     c=0
-    a=0
-	print get_cpu_usage()
+    mannds=0
+    print get_cpu_usage()
 	print get_io_usage()
     
     pass
